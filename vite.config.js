648e3db0e2cd53f988293ccabb8cb5554b31b389
JC_SOHAN\sohan <sohan.varnikar@juliacomputing.com>
2026-03-17 02:35:19 +0530

    fix: fixes - corrected some duplicate lines - vite update to v8 - add .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: ".",
+  publicDir: "public",
   server: {
     port: 5173,
-    open: true
-  }
-})
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
-  root: '.',
-  publicDir: 'public',
+    open: true,
+  },
 });
+
+// import vue from '@vitejs/plugin-vue';
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   root: '.',
+//   publicDir: 'public',
+// });
